800e1eb164da52910387f3a64afd7878d42b1103
wazunon <guchi5276@gmail.com>
2024-11-18 18:35:42 +0900

    タイトルを変更

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,10 +8,10 @@ import styles from './layout.module.css';
 
 export const metadata = {
   metadataBase: new URL(process.env.BASE_URL || 'http://localhost:3000'),
-  title: 'Simple Blog',
+  title: '4eng',
   description: 'A simple blog presented by microCMS',
   openGraph: {
-    title: 'Simple Blog',
+    title: '4eng',
     description: 'A simple blog presented by microCMS',
     images: '/ogp.png',
   },
